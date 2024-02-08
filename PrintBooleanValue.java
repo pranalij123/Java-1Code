5b2fab2f9bba0bb5da6de7a20922750f93419954
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-08 11:26:22 +0530

    Print boolean value

---
+++ PrintBooleanValue.java
@@ -0,0 +1,11 @@
+class PrintBooleanValue {
+    public static void main(String[] args) {
+        boolean a=true;
+        System.out.println(a);
+    }
+}
+
+/* Output:
+ Compile time:
+ Run time:
+ */
