ee0720108c21adcc19ac8038eb03963241b08682
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-08 11:18:49 +0530

    Print char value

---
+++ PrintCharValue.java
@@ -0,0 +1,14 @@
+class PrintCharValue {
+    public static void main(String [] args)
+    {
+        char a='A';
+        System.out.println(a);
+    }
+}
+
+
+
+/*output:
+ compile time:
+ Run time: 
+ */
