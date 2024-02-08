6805f47f84bcfb07588535068b389aec794ec8fb
pranalij123 <jadhavpranali481@gmail.com>
2024-02-08 10:51:16 +0530

    Print float value

---
+++ PrintFloatValue.java
@@ -0,0 +1,13 @@
+class PrintFloatValue {
+    public static void main(String [] args)
+    {
+        float a=12.1f;
+        System.out.println(a);
+    }
+}
+
+
+/*output:
+ compile time:
+ Run time: 
+ */
