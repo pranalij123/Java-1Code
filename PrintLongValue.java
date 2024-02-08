b86f89f47a483504a7be38b60e9966f3796bf78c
pranalij123 <jadhavpranali481@gmail.com>
2024-02-08 10:42:15 +0530

    code to print Long value

---
+++ PrintLongValue.java
@@ -0,0 +1,13 @@
+class PrintLongValue {
+    public static void main(String [] args)
+    {
+        long a=1234567890;
+        System.out.println(a);
+    }
+}
+
+
+/*output:
+ compile time: 
+ Run time: 
+ */
