b770d04bbd8e1ec5dbcbd1616b187cf6e4e62e10
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-09 13:32:49 +0530

    Addition of  values with different data types

---
+++ AdditionOfDataTypes.java
@@ -0,0 +1,42 @@
+class AdditionOfDataTypes{
+public static void main(String [] aofdt){
+
+char a='A';//ASCII (American Standard Code for Information Interchange) value of 'A' is 65
+int b=3;
+System.out.println(a+b);
+}
+}
+/*Output:
+comile time:success
+Run time:68
+*/
+
+/* Another conversions of different data types
+byte + byte = int
+short + short = int
+byte + short = int
+byte + int = int
+byte + long = long
+byte + float = float
+byte + double = double
+byte + char = int 
+byte + boolean = Error (boolean does not convert in another data type)
+short + short = int
+short + char = int
+short + int = int
+short + long = long
+short + float = float
+short + double = double
+int + float = float
+int + long = long
+int + double = double
+int + char = int
+long + float = float
+long + double = double
+long + char = long
+float + double = double
+float + char =float
+double + char = double
+char + char = int
+*/ 
+
