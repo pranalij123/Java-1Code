1bbdf8d6cd3de5bd87f218182a35b1f5e3c79991
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-09 13:05:55 +0530

    print  Last Digit Of Number

---
+++ LastDigitOfNumber.java
@@ -0,0 +1,12 @@
+class LastDigitOfNumber{
+public static void main(String[] ldon){
+int number=1234567;
+int last_Digit=number%10;
+System.out.println(last_Digit);
+
+}
+}
+/*output:
+compile time:success;
+Run time:7
+*/
