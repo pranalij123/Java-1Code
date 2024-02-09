a8b14db8fdd90e21d3db001a4424ccfaaae61ee0
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-09 13:07:26 +0530

    print last two digit of number

--- LastDigitOfNumber.java
+++ LastDigitOfNumber.java
@@ -1,12 +1,13 @@
 class LastDigitOfNumber{
 public static void main(String[] ldon){
 int number=1234567;
-int last_Digit=number%10;
+  //print last two digit of number
+int last_Digit=number%100;
 System.out.println(last_Digit);
 
 }
 }
 /*output:
 compile time:success;
-Run time:7
+Run time:67
 */
