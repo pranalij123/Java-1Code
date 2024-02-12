8739bb4b6d1243c01eb749c9b6e08ccec9abc70e
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-12 23:04:24 +0530

    Addition Of Natural No

---
+++ AdditionOfNatuaralNo.java
@@ -0,0 +1,16 @@
+class AdditionOfNatuaralNo{
+public static void main(String [] aonn){
+int n=20;
+int result=((n*(n+1))/2);
+System.out.println("addition of "+n+" natuaral number is "+result);
+boolean a=(result%2==0);
+System.out.println("Sum of natural number upto "+n+ " is even:"+ a);
+
+}
+}
+/*Output:
+comile time:success
+Run time:210
+*/
+
+
