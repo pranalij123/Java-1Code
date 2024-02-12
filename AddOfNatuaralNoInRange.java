394e0b0761d3ea20ef8340e50039acdec0e17259
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-12 23:17:14 +0530

    Addition Of Natural No In given Range

---
+++ AddOfNatuaralNoInRange.java
@@ -0,0 +1,18 @@
+class AddOfNaturalNoInRange{
+public static void main(String [] aonn){
+int a=5;
+int b=15;
+int c=((b*(b+1))/2);
+int d=((a*(a-1))/2);
+//Addition of natural number in given range
+int result=c-d;
+System.out.println("Addintion of natural number in given range "+"("+a+","+b+")"+"is "+result); 
+
+}
+}
+/*Output:
+comile time:success
+Run time:110
+*/
+
+
