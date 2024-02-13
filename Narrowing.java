b5d4e5d0611ba76ad95718b28d697b61dbc5eb19
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-14 00:43:50 +0530

    Narrowing  type casting

---
+++ Narrowing.java
@@ -0,0 +1,13 @@
+class Narrowing{
+public static void main(String []args)
+{
+//narrowing is used to convert lager type of data to smaller type
+int temp=152;
+byte temp1=(byte)temp;
+System.out.println(temp1);
+
+}
+}
+
+//output:-104
+//while using narrowing there may be data loss
