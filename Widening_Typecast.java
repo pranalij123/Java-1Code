548cc12299077cd1babfcab38b6686d99ab66209
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-14 00:33:36 +0530

    Widening Typecast

---
+++ Widening_Typecast.java
@@ -0,0 +1,12 @@
+class Widening_Typecast{
+public static void main(String [] args){
+int a=10;
+float b=a;
+System.out.println(b);//implicite type casting, output:10.0
+
+int a1=10;
+float b1=(float)a1;
+System.out.println(b1);//10.0
+
+}
+}
