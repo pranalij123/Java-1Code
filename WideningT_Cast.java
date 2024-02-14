66950a2d8193d5e9877668db02c7e7033ba210b6
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-15 00:06:39 +0530

    Widening Type Cast

---
+++ WideningT_Cast.java
@@ -0,0 +1,15 @@
+class WideningT_Cast{
+
+public static void main(String []args)
+{
+short age=1235;
+double age1=(double) age;
+System.out.println(age1);
+
+}
+}
+
+/*output:
+compile time: Success
+Run time :1235.0
+*/
