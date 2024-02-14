28b9d1253a5325ec40ec7cb0d534c19c46ae6b48
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-15 00:10:29 +0530

    Create WideningT_Cast1.java

---
+++ WideningT_Cast1.java
@@ -0,0 +1,13 @@
+class WideningT_Cast1{
+public static void main(String []args)
+{
+char a='@';//ASCII value 64
+double b=(double)a;
+System.out.println(b);
+
+}
+}
+/*Output:
+compile time:success
+Run time:64.0
+*/
