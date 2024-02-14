5179854ac5566d30158e6ba9f7f7d1a439343ded
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-15 00:15:24 +0530

    Check Number is Odd or not

---
+++ NumberOdd.java
@@ -0,0 +1,12 @@
+class NumberOdd{
+public static void main(String []args)
+{
+char a='@';//ASCII value 64
+boolean n=(a%2==1);//64%2==0
+System.out.println("Number is odd:"+n);
+}
+}
+/*OUTPUT:
+compile time:success
+Run time:Number is odd:false
+*/
