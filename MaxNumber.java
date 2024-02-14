1a7c2527b9b9c06ed7a75620b45e8c16e1aa0b18
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-15 00:25:18 +0530

    find max number using ternary operator

---
+++ MaxNumber.java
@@ -0,0 +1,13 @@
+class MaxNumber{
+public static void main(String []args)
+{
+int a=20;
+int b=30;
+String max=a>b? ("a is greater"):("b is greater");//20>30 is false hence it will print second term
+System.out.println(max);
+}
+}
+/*output:
+compile time:success
+run time:b is greater
+*/
