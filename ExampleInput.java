81bf201fa5621b70b0e2764ae01c7e50b77678a9
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-21 12:59:12 +0530

    Run time input

---
+++ ExampleInput.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class ExampleInput{
+
+public static void main(String []args)
+{
+//object of class Scanner
+Scanner sc= new Scanner(System.in);
+System.out.println("enter the int value");
+//Run time input
+int b= sc.nextInt();
+System.out.println("The value is "+b);
+}
+}
+
+/* OUTPUT:
+compile time:success;
+run time:enter the int value
+56
+The value is 56
+*/
