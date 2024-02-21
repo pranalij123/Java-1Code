99fe5682bd0e6aa386acacec13cc483de470681b
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-21 13:26:26 +0530

    Create InputString.java

---
+++ InputString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class InputString{
+
+public static void main(String []args)
+{
+
+Scanner sc= new Scanner(System.in);
+System.out.println("enter the your name ");
+String name= sc.next();//inbuilt method to accept input as a string which does not include space
+System.out.println(name);
+}
+}
+
+/*Output :
+compile time:success;
+Run time:enter the your name
+Neha
+Neha
+
+*/
