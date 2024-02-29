7fa5d6c88522c006e61e8f44168278e2553ea07c
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-02-29 18:45:07 +0530

    find marks with the help grade using switch

---
+++ MarksUsingSwitch.java
@@ -0,0 +1,28 @@
+class PrintLongValue {
+    public static void main(String [] args)
+    {
+         char grade='A';
+        switch(grade){
+            case 'A':
+            System.out.println("marks is within 80 to 100");
+            break;
+
+            case 'B':
+            System.out.println("marks is within 60 to 80");
+            break;
+            case 'C':
+            System.out.println("marks is within 40 to 60");
+            break;
+            case 'E':
+            System.out.println("marks is within 35 to 40");
+            break;
+            case 'F':
+            System.out.println("marks is within 0 to 35");
+            break;
+
+
+        }
+    }
+}
+
+
