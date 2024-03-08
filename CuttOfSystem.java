d927aee183e01e533249ab407726aec9b282e7ce
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-03-08 12:15:00 +0530

    Admission criteria using switch

---
+++ CuttOfSystem.java
@@ -0,0 +1,295 @@
+import java.util.Scanner;
+
+
+class CuttOfSystem{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" 1:Remote  2:India 3:World");
+        System.out.println("enter Place");
+        int a= sc.nextInt();
+        switch(a){
+            case 1:System.out.println("1:Dattakala  2:Apcoer 3:GH Raisoni");
+                 System.out.println("enter College");
+                 int b=sc.nextInt();
+                 switch (b) {
+                    case 1:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+                        System.out.println("enter number according to condition ");
+                        int r=sc.nextInt();
+                        switch (r) {
+                            case 1:System.out.println("grade is C");
+                            System.out.println("You can  take admission in this college ");
+                                break;
+                        
+                               case 2:System.out.println("grade is B");
+                               System.out.println("You can  take admission in this college ");
+                                break;
+                        
+                                case 3:System.out.println("grade is A");
+                            System.out.println("You can take admission in this college ");
+                                break;
+                        
+                            default:
+                            System.out.println("invalisd input");
+                                
+                        }
+                        break;
+                       
+                 
+                 
+
+
+                 case 2:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+                        System.out.println("enter number according to condition ");
+                        int c=sc.nextInt();
+                        switch (c) {
+                            case 1:System.out.println("grade is C");
+                            System.out.println("You can not  take admission in this college ");
+                                break;
+                        
+                               case 2:System.out.println("grade is B");
+                               System.out.println("You can  take admission in this college ");
+                                break;
+                        
+                                case 3:System.out.println("grade is A");
+                            System.out.println("You can take admission in this college ");
+                                break;
+                        
+                            default:
+                            System.out.println("invalisd input");
+                                
+
+                     
+                                
+                        }
+                        break;
+                        
+                 
+                    
+                 
+                
+
+
+                 case 3:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+                        System.out.println("enter number according to condition ");
+                        int p=sc.nextInt();
+                        switch (p) {
+                            case 1:System.out.println("grade is C");
+                            System.out.println("You can not take admission in this college ");
+                                break;
+                        
+                               case 2:System.out.println("grade is B");
+                               System.out.println("You can not take admission in this college ");
+                                break;
+                        
+                                case 3:System.out.println("grade is A");
+                            System.out.println("You can take admission in this college ");
+                                break;
+                        
+                            default:
+                            System.out.println("invalisd input");
+                                
+                        }
+                        
+                break;
+        }
+break;
+        
+
+        case 2:System.out.println("1:MIT  2:COEP 3:PCCOE");
+        System.out.println("enter College");
+        int t=sc.nextInt();
+        switch (t) {
+           case 1:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+               System.out.println("enter number according to condition ");
+               int r=sc.nextInt();
+               switch (r) {
+                   case 1:System.out.println("grade is C");
+                   System.out.println("You can not take admission in this college ");
+                       break;
+               
+                      case 2:System.out.println("grade is B");
+                      System.out.println("You can  take admission in this college ");
+                       break;
+               
+                       case 3:System.out.println("grade is A");
+                   System.out.println("You can take admission in this college ");
+                       break;
+               
+                   default:
+                   System.out.println("invalisd input");
+                       
+               }
+               break;
+              
+        
+        
+
+
+        case 2:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+               System.out.println("enter number according to condition ");
+               int c=sc.nextInt();
+               switch (c) {
+                   case 1:System.out.println("grade is C");
+                   System.out.println("You can not  take admission in this college ");
+                       break;
+               
+                      case 2:System.out.println("grade is B");
+                      System.out.println("You can  take admission in this college ");
+                       break;
+               
+                       case 3:System.out.println("grade is A");
+                   System.out.println("You can take admission in this college ");
+                       break;
+               
+                   default:
+                   System.out.println("invalisd input");
+                       
+
+            
+                       
+               }
+               break;
+               
+        
+           
+        
+       
+
+
+        case 3:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+               System.out.println("enter number according to condition ");
+               int p=sc.nextInt();
+               switch (p) {
+                   case 1:System.out.println("grade is C");
+                   System.out.println("You can not take admission in this college ");
+                       break;
+               
+                      case 2:System.out.println("grade is B");
+                      System.out.println("You can  take admission in this college ");
+                       break;
+               
+                       case 3:System.out.println("grade is A");
+                   System.out.println("You can take admission in this college ");
+                       break;
+               
+                   default:
+                   System.out.println("invalisd input");
+                       
+               }
+               
+       break;
+}
+ break;
+
+
+ case 3:System.out.println("1:UCB  2:NUS 3:COEP");
+ System.out.println("enter College");
+ int w=sc.nextInt();
+ switch (w) {
+    case 1:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+        System.out.println("enter number according to condition ");
+        int r=sc.nextInt();
+        switch (r) {
+            case 1:System.out.println("grade is C");
+            System.out.println("You can not take admission in this college ");
+                break;
+        
+               case 2:System.out.println("grade is B");
+               System.out.println("You can not take admission in this college ");
+                break;
+        
+                case 3:System.out.println("grade is A");
+            System.out.println("You can take admission in this college ");
+                break;
+        
+            default:
+            System.out.println("invalisd input");
+                
+        }
+        break;
+       
+ 
+ 
+
+
+ case 2:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+        System.out.println("enter number according to condition ");
+        int c=sc.nextInt();
+        switch (c) {
+            case 1:System.out.println("grade is C");
+            System.out.println("You can not take admission in this college ");
+                break;
+        
+               case 2:System.out.println("grade is B");
+               System.out.println("You can not take admission in this college ");
+                break;
+        
+                case 3:System.out.println("grade is A");
+            System.out.println("You can take admission in this college ");
+                break;
+        
+            default:
+            System.out.println("invalisd input");
+                
+
+     
+                
+        }
+        break;
+        
+ 
+    
+ 
+
+
+
+ case 3:System.out.println("1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks");
+        System.out.println("enter number according to condition ");
+        int p=sc.nextInt();
+        switch (p) {
+            case 1:System.out.println("grade is C");
+            System.out.println("You can not take admission in this college ");
+                break;
+        
+               case 2:System.out.println("grade is B");
+               System.out.println("You can not take admission in this college ");
+                break;
+        
+                case 3:System.out.println("grade is A");
+            System.out.println("You can take admission in this college ");
+                break;
+        
+            default:
+            System.out.println("invalisd input");
+                
+        }
+        
+break;
+}
+break;
+
+
+default:System.out.println("enter valid input");
+    }
+    }
+}
+
+
+
+/*output:
+ 1:Remote  2:India 3:World
+enter Place
+2
+1:MIT  2:COEP 3:PCCOE
+enter College
+3
+1: 10>=marks && 30<=marks  2:30<=marks && 60<=marks  3: 60<=marks && 100<=marks
+enter number according to condition
+3
+grade is A
+You can take admission in this college
+
+
+
+*/
